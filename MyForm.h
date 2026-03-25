c74ff8ea6c78346c5528c227e9a6d0397ef3e64b
IrynaPaplinska <paplinskairina12@gmail.com>
2026-03-25 11:28:00 +0200

    sum, min, max

--- MyForm.h
+++ MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+
 int mas1[5];        // одновимірний масив
 int matrix[5][5];   // двовимірний масив
 namespace Project1 {
@@ -36,6 +37,7 @@ namespace Project1 {
 	private: System::Windows::Forms::ComboBox^ comboBox1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Label^ label1;
 
 	protected:
 
@@ -58,6 +60,7 @@ namespace Project1 {
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -81,7 +84,7 @@ namespace Project1 {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(468, 310);
+			this->button1->Location = System::Drawing::Point(687, 310);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(142, 65);
 			this->button1->TabIndex = 1;
@@ -99,6 +102,16 @@ namespace Project1 {
 			this->dataGridView1->Size = System::Drawing::Size(538, 182);
 			this->dataGridView1->TabIndex = 2;
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(244, 331);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 20);
+			this->label1->TabIndex = 3;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
@@ -107,6 +120,7 @@ namespace Project1 {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->ClientSize = System::Drawing::Size(934, 428);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->comboBox1);
@@ -114,6 +128,7 @@ namespace Project1 {
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 
@@ -134,18 +149,35 @@ namespace Project1 {
 		}
 		if (comboBox1->SelectedIndex == 1) // Сума
 		{
-			//TODO: Сума елементів масиву
+			int sum = 0;
+			for (int i = 0; i < 5; i++)
+			{
+				sum += mas1[i];
+			}
+			label1->Text = "Сума елементів масиву: " + sum.ToString();
 		}
-
+		
 		if (comboBox1->SelectedIndex == 2) // Мінімум
 		{
-		//TODO: Мінімальний елемент масиву
+			int min = mas1[10];
+			for (int i = 0; i < 5; i++)
+			{
+				if (mas1[i] < min)
+					min = mas1[i];
+			}
+			label1->Text = "Мінімальний елемент масиву: " + min.ToString();
 			
 		}
 
 		if (comboBox1->SelectedIndex == 3) // Максимум
 		{
-			//TODO: Максимальний елемент масиву
+			int max = mas1[10];
+			for (int i = 0; i < 5; i++)
+			{
+				if (mas1[i] > max)
+					max = mas1[i];
+			}
+			label1->Text = "Максимальний елемент масиву: " + max.ToString();
 		}
 
 		if (comboBox1->SelectedIndex == 4) // Парні/непарні
